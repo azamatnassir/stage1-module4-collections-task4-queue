98300dc728ec6951bf40045237b7519c30a7b714
azamatnassir <azamatnsr@gmail.com>
2023-08-09 15:57:11 +0600

    commit

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,29 @@
 package com.epam.collections.queue;
 
 import java.util.ArrayDeque;
+import java.util.Objects;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> result = new ArrayDeque<>();
+        result.offerLast(Objects.requireNonNull(firstQueue.poll()));
+        result.offerLast(Objects.requireNonNull(firstQueue.poll()));
+        result.offerLast(Objects.requireNonNull(secondQueue.poll()));
+        result.offerLast(Objects.requireNonNull(secondQueue.poll()));
+
+        while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
+
+            firstQueue.add(result.pollLast());
+            result.offerLast(Objects.requireNonNull(firstQueue.poll()));
+            result.offerLast(Objects.requireNonNull(firstQueue.poll()));
+
+            secondQueue.add(result.pollLast());
+            result.offerLast(Objects.requireNonNull(secondQueue.poll()));
+            result.offerLast(Objects.requireNonNull(secondQueue.poll()));
+        }
+
+        return result;
     }
 }
--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -1,9 +1,18 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        List<String> merged = new ArrayList<>(firstList);
+        merged.addAll(secondList);
+
+        PriorityQueue<String> queue = new PriorityQueue<>(Comparator.reverseOrder());
+        queue.addAll(merged);
+
+        return queue;
     }
 }
